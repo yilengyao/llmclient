45a485c91029be4e74b6d0c9757b80932772b693
yilengyao <innobridgetechnology@gmail.com>
2025-04-26 22:43:29 +0000

    Initialize TypeScript library with tsdx

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true,
+  entry: ['./src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  treeshake: true,
+  cjsInterop: true,
+});
